c8691101a746a1f59f75025d5a80cd77c12605d1
Kaitlyn <jennkait@gmail.com>
2019-09-17 14:46:03 -0400

    day 7

---
+++ day7.js
@@ -0,0 +1,60 @@
+//Day 7 Assignment
+var groceries = {
+    inventory: {},
+    fillArray: function(){
+        this.inventory["water"] = 100,
+        this.inventory["soda"] = 100,
+        this.inventory["detergent"] = 50,
+        this.inventory["apple"] = 100,
+        this.inventory["banana"] = 100,
+        this.inventory["tomato"] = 100,
+        this.inventory["bread"] = 50,
+        this.inventory["butter"] = 50,
+        this.inventory["milk"] = 50,
+        this.inventory["chicken"] = 50;
+    },
+    addInventory: function(itemName,quantity){
+        this.inventory[itemName] += quantity;
+    },
+    
+    subrtractInventory: function(itemName,quantity){
+        this.inventory[itemName] -= quantity;
+    },
+    
+};
+
+var inventory = [];
+function fillArray() {
+        inventory["water"] = 100,
+        inventory["soda"] = 100,
+        inventory["detergent"] = 50,
+        inventory["apple"] = 100,
+        inventory["banana"] = 100,
+        inventory["tomato"] = 100,
+        inventory["bread"] = 50,
+        inventory["butter"] = 50,
+        inventory["milk"] = 50,
+        inventory["chicken"] = 50;
+ }
+
+ fillArray();
+
+
+ function addInventory(itemName, quantity) {
+    inventory[itemName] += quantity;
+ }
+
+ addInventory("water",100)
+
+ function subtractInventory(itemName, quantity){
+     inventory[itemName] -= quantity;
+ }
+ subtractInventory("soda",100)
+
+ function outOfStock(){
+    for (let item in inventory) {
+    if(inventory[itemName] === 0) {
+      console.log([itemName] + "is out of stock");
+      }
+    }
+ }
