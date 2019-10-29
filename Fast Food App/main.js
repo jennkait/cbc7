5df8266519a27aaa824bcd3f03bb4bc58e399802
Kaitlyn <jennkait@gmail.com>
2019-10-28 22:11:22 -0400

    Fast Food App

---
+++ Fast Food App/main.js
@@ -0,0 +1,48 @@
+var orderInput = document.getElementById('order-input');
+var menuList = document.getElementById('menu-list');
+// var customerOrder;
+var menu = {"fries": 20, "hamburgers": 10, "shakes": 5 }
+
+function setMenu() {
+    var menuString = '';
+    for (var name in menu) {
+        menuString += `<li>${name} - ${menu[name]}</li>`;
+    }
+    menuList.innerHTML = menuString;
+}
+
+function placeOrder(){
+    
+    console.log('orderInput',orderInput.value)
+
+    // parse comma delimited order
+    var items = orderInput.value.split(',');
+
+    // loop through order
+    for (var item of items) {
+        // parse item quantity from name and trim
+        item = item.split(':')
+        var quantity = item[0];
+        var name = item[1];
+        quantity = quantity.trim();
+        name = name.trim();
+        name = name.toLowerCase();
+
+        console.log('name', name)
+        console.log('quantity', quantity)
+
+        if (!menu[name]) {
+            // not on menu or not enough quantity
+            console.log(`We do not carry ${name}`);
+        } else if (menu[name] - quantity < 0) {
+            console.log(`We are out of stock of ${name}`);
+        }else {
+            // item is in stock
+            menu[name] = menu[name] - quantity;
+            console.log(`Purchased ${quantity} ${name}`);
+        }
+        orderInput.value = '';
+        setMenu();
+    }
+}
+ setMenu();
