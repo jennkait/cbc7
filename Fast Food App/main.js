857b56b8ed0c89f703456f489727eeaf24cb9d6a
Kaitlyn <jennkait@gmail.com>
2019-10-30 23:24:47 -0400

    Backend

--- Fast Food App/main.js
+++ Fast Food App/main.js
@@ -44,4 +44,15 @@ function placeOrder(){
         setMenu();
     }
 }
- setMenu();
+setMenu();
+ 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+ };
+xhttp.open("POST", "demo_post.asp", true);
+xhttp.send();
+}
