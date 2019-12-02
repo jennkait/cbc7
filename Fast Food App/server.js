c5ebe64256754cea6ca4a90c2fb52398813eb52e
Kaitlyn <jennkait@gmail.com>
2019-12-02 15:41:47 -0500

    Database

--- Fast Food App/server.js
+++ Fast Food App/server.js
@@ -1,10 +1,14 @@
 var express = require("express")
 var app = express()
+var db = require("./db");
+
 app.get ("/", (req,res) => {
     res.send ("fastfood")
+    db.query(console.log ("Connected!"));
 })
 app.post ("/send-order", (req,res) => {
     console.log (req.body)
     res.send ("fastfood")
 })
 app.listen(8080)
+
